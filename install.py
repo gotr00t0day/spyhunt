d629dee422e175228f30b88749718feeb799ffcf
Miguel Sanchez <c0d3ninja@Luisas-iMac.local>
2024-08-08 00:28:53 -0400

    The installation script is now cross platform

--- install.py
+++ install.py
@@ -1,159 +1,141 @@
-from shutil import which
-from colorama import Fore, Back, Style
-from os import path
-import os.path
-import os
+import platform
 import subprocess
+import os
+from shutil import which
+from colorama import Fore, init
 
-def commands(cmd):
+init(autoreset=True)
+
+def run_command(cmd):
     try:
-        subprocess.check_call(cmd, shell=True)
-    except:
-        pass
+        return subprocess.check_output(cmd, shell=True, stderr=subprocess.STDOUT, universal_newlines=True)
+    except subprocess.CalledProcessError as e:
+        print(f"{Fore.RED}Failed to run command: {cmd}")
+        print(f"Error: {e.output}")
+        return None
 
-# colorama 
-commands("sudo pip3 install colorama")
-
-# golang
-commands("sudo apt install golang")
-
-# nodejs
-commands("sudo apt install nodejs")
-if which("nodejs"):
-    print(Fore.GREEN + "Found nodejs")
+def detect_package_manager():
+    package_managers = [
+        ("apt", "apt"),
+        ("dnf", "dnf"),
+        ("yum", "yum"),
+        ("pacman", "pacman"),
+        ("zypper", "zypper"),
+        ("apk", "apk")
+    ]
     
-# npm
-commands("sudo apt install npm")
-if which("npm"):
-    print(Fore.GREEN + "Found npm")
+    for pm, cmd in package_managers:
+        if which(cmd):
+            return pm
     
-# brokenlinkchecker
-if which("blc"):
-    pass
-else:
-    commands("npm install broken-link-checker -g")
-    if which("blc"):
-        print(Fore.GREEN + "broken-link-checker installed successfully")
+    return None
 
-home = os.environ['HOME']
+def install_package(package, manager):
+    if manager == "apt":
+        return run_command(f"sudo apt install -y {package}")
+    elif manager == "dnf" or manager == "yum":
+        return run_command(f"sudo {manager} install -y {package}")
+    elif manager == "pacman":
+        return run_command(f"sudo pacman -S --noconfirm {package}")
+    elif manager == "zypper":
+        return run_command(f"sudo zypper install -y {package}")
+    elif manager == "apk":
+        return run_command(f"sudo apk add {package}")
+    elif manager == "brew":
+        return run_command(f"brew install {package}")
+    elif manager == "pip":
+        return run_command(f"pip3 install {package}")
+    elif manager == "npm":
+        return run_command(f"sudo npm install -g {package}")
+    elif manager == "go":
+        return run_command(f"go install {package}")
 
-# This will install nuclei
+def install_tool(name, install_cmd, check_cmd=None):
+    if check_cmd is None:
+        check_cmd = name
+    if not which(check_cmd):
+        print(f"{Fore.YELLOW}Installing {name}...")
+        result = install_cmd()
+        if result is not None:
+            print(f"{Fore.GREEN}{name} installed successfully")
+        else:
+            print(f"{Fore.RED}Failed to install {name}")
+    else:
+        print(f"{Fore.GREEN}Found {name}")
 
-commands("go install -v github.com/projectdiscovery/nuclei/v2/cmd/nuclei@latest")
+def main():
+    system = platform.system()
+    if system == "Linux":
+        package_manager = detect_package_manager()
+        if package_manager is None:
+            print(f"{Fore.RED}Unable to detect package manager. Please install packages manually.")
+            return
+        print(f"{Fore.GREEN}Detected package manager: {package_manager}")
+    elif system == "Darwin":  # macOS
+        package_manager = "brew"
+        if not which("brew"):
+            print(f"{Fore.RED}Homebrew is required for macOS. Please install it first.")
+            return
+    else:
+        print(f"{Fore.RED}Unsupported operating system: {system}")
+        return
 
-# Make sure nuclei-templates is in the / directory
+    home = os.path.expanduser("~")
+    
+    # Install colorama
+    install_tool("colorama", lambda: install_package("colorama", "pip"))
 
-commands("cd ~ | git clone https://github.com/projectdiscovery/nuclei-templates.git")
+    # Install golang
+    install_tool("go", lambda: install_package("golang", package_manager))
 
-#dnsx
-if which("dnsx"):
-    pass
-else:
-    commands("go install github.com/projectdiscovery/dnsx/cmd/dnsx@latest | cd {}/go/bin | sudo mv {}/go/bin/dnsx /usr/local/bin".format(home, home))
-    if which("dnsx"):
-        print(Fore.GREEN + "dnsx installed successfully")
+    # Install nodejs and npm
+    install_tool("node", lambda: install_package("nodejs", package_manager))
+    install_tool("npm", lambda: install_package("npm", package_manager))
 
-#aquatone
-if which("aquatone"):
-    pass
-else:
-    filepath = os.path.abspath(os.getcwd())
-    commands("wget -O aquatone.zip https://github.com/michenriksen/aquatone/releases/download/v1.7.0/aquatone_linux_amd64_1.7.0.zip | sudo mv {}/aquatone /usr/local/bin".format(filepath))
-    if which("aquatone"):
-        print(Fore.GREEN + "aquatone installed successfully")
+    # Install broken-link-checker
+    install_tool("blc", lambda: install_package("broken-link-checker", "npm"))
 
-#subfinder
-if which("subfinder"):
-    pass
-else:
-    commands("go install github.com/projectdiscovery/subfinder/v2/cmd/subfinder@latest | cd {}/go/bin | sudo mv {}/go/bin/subfinder /usr/local/bin".format(home, home))
-    if which("subfinder"):
-        print(Fore.GREEN + "subfinder installed successfully")
+    # Install nuclei
+    install_tool("nuclei", lambda: run_command("go install -v github.com/projectdiscovery/nuclei/v2/cmd/nuclei@latest"))
 
-#jq
-if which("jq"):
-    pass
-else:
-    commands("sudo apt install jq")
-    if which("jq"):
-        print(Fore.GREEN + "jq installed successfully")
+    # Clone nuclei-templates
+    if not os.path.exists(os.path.join(home, "nuclei-templates")):
+        run_command(f"git clone https://github.com/projectdiscovery/nuclei-templates.git {home}/nuclei-templates")
 
-#waybackurls
-if which("waybackurls"):
-    pass
-else:
-    commands("go install github.com/tomnomnom/waybackurls@latest | cd {}/go/bin | sudo mv {}/go/bin/waybackurls /usr/local/bin".format(home, home))
-    if which("waybackurls"):
-        print(Fore.GREEN + "installed successfully")
+    # Install other tools
+    tools = [
+        ("dnsx", "github.com/projectdiscovery/dnsx/cmd/dnsx@latest"),
+        ("subfinder", "github.com/projectdiscovery/subfinder/v2/cmd/subfinder@latest"),
+        ("waybackurls", "github.com/tomnomnom/waybackurls@latest"),
+        ("httprobe", "github.com/tomnomnom/httprobe@latest"),
+        ("httpx", "github.com/projectdiscovery/httpx/cmd/httpx@latest"),
+        ("anew", "github.com/tomnomnom/anew@latest"),
+        ("gau", "github.com/lc/gau/v2/cmd/gau@latest"),
+        ("gauplus", "github.com/bp0lr/gauplus@latest"),
+        ("hakrawler", "github.com/hakluke/hakrawler@latest"),
+        ("assetfinder", "github.com/tomnomnom/assetfinder@latest"),
+    ]
 
-#httprobe
-if which("httprobe"):
-    pass
-else: 
-    commands("go install github.com/tomnomnom/httprobe@latest | cd {}/go/bin | sudo mv {}/go/bin/httprobe /usr/local/bin".format(home, home))
+    for tool, go_package in tools:
+        install_tool(tool, lambda t=tool, p=go_package: install_package(p, "go") and run_command(f"sudo mv $(go env GOPATH)/bin/{t} /usr/local/bin/"))
 
-#httpx
-if which("httpx"):
-    pass
-else: 
-    commands("go install github.com/projectdiscovery/httpx/cmd/httpx@latest | cd {}/go/bin | sudo mv {}/go/bin/httpx /usr/local/bin".format(home, home))
-    if which("httpx"):
-        print(Fore.GREEN + "httpx installed successfully")
+    # Install jq
+    install_tool("jq", lambda: install_package("jq", package_manager))
 
-#anew
-if which("anew"):
-    pass
-else:
-    commands("go install github.com/tomnomnom/anew@latest | cd {}/go/bin | sudo mv {}/go/bin/anew /usr/local/bin".format(home, home))
-    if which("anew"):
-        print(Fore.GREEN + "anew installed successfully")
+    # Install aquatone
+    if not which("aquatone"):
+        run_command("wget -O aquatone.zip https://github.com/michenriksen/aquatone/releases/download/v1.7.0/aquatone_linux_amd64_1.7.0.zip")
+        run_command("unzip aquatone.zip")
+        run_command("sudo mv aquatone /usr/local/bin/")
+        run_command("rm aquatone.zip")
+        print(f"{Fore.GREEN}aquatone installed successfully")
 
-#gau
-if which("gau"):
-    pass
-else:
-    commands("go install github.com/lc/gau@latest | cd {}/go/bin | sudo mv {}/go/bin/gau /usr/local/bin".format(home, home))
-    if which("gau"):
-        print(Fore.GREEN + "gau installed successfully")
+    # Install shodan
+    install_tool("shodan", lambda: install_package("shodan", "pip"))
 
-#gauplus
-if which("gauplus"):
-    pass
-else:
-    commands("go install github.com/bp0lr/gauplus@latest | cd {}/go/bin | sudo mv {}/go/bin/gauplus /usr/local/bin".format(home, home))
-    if which("gauplus"):
-        print(Fore.GREEN + "gauplus installed successfully")
+    # Install paramspider
+    install_tool("paramspider", lambda: run_command("git clone https://github.com/devanshbatham/paramspider && cd paramspider && python3 setup.py install"))
 
-#hakrawler
-if which("hakrawler"):
-    pass
-else:  
-    commands("go install github.com/hakluke/hakrawler@latest | cd {}/go/bin | sudo mv {}/go/bin/hakrawler /usr/local/bin".format(home, home))
-    if which("gau"):
-        print(Fore.GREEN + "gau installed successfully")
-
-#shodan
-if which("shodan"):
-    pass
-else:
-    commands("easy_install shodan")
-    if which("shodan"):
-        print(Fore.GREEN + "shodan installed successfully")
-
-#paramspider
-if which("paramspider"):
-    pass
-else:
-    commands(r"git clone https://github.com/devanshbatham/paramspider | cd paramspider | python3 setup.py install")
-    if which("paramspider"):
-        print(Fore.GREEN + " installed successfully")
-
-#assetfinder
-if which("assetfinder"):
-    pass
-else:
-    commands("go install github.com/tomnomnom/assetfinder@latest | cd {}/go/bin | sudo mv {}/go/bin/assetfinder /usr/local/bin".format(home, home))
-    if which("assetfinder"):
-        print(Fore.GREEN + "assetfinder installed successfully")
-
+if __name__ == "__main__":
+    main()
 
